67ba3c46a5811412c57aae661e63b037a74c3b5a
にゅ <nyu@MacBookAir.lan>
2026-06-06 23:25:54 -0700

    Fix Express 5 wildcard routing syntax for static files

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/api', apiRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get('/(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
